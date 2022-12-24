a6f1ed5e4435f8520ea0e39d3ac9766c38f88269
Anton Orlov <orlov88@gmail.com>
2022-12-24 13:25:04 +0300

    Dockerization

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Hat-api-gateway
+Требуемая версия JAVA 17. 
+
+* Конфигурация путей задана в `application.yaml`
+
+## Локальный запуск
+
+### Опция #1
+В IDE запустите `rubber.dutch.hat.apigateway.ApiGatewayApplication`
+
+### Опция #2
+Запуск из консоли командой `./gradlew bootRun`
+
+## Docker
+* Сборка образа: `docker build -t hat-api-gateway .`
+* Запуск контейнера: `docker run -p 9002:9002 hat-api-gateway`
--- src/main/kotlin/rubber/dutch/hat/apigateway/ApiGatewayApplication.kt
+++ src/main/kotlin/rubber/dutch/hat/apigateway/ApiGatewayApplication.kt
@@ -1,11 +1,15 @@
 package rubber.dutch.hat.apigateway
 
+import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ApiGatewayApplication
+class ApiGatewayApplication {
 
-fun main(args: Array<String>) {
-    runApplication<ApiGatewayApplication>(*args)
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(ApiGatewayApplication::class.java, *args)
+        }
+    }
 }
