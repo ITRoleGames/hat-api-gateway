ff51fce1e7e040b6aaaeda62ef28a9b06c926048
Anton Orlov <orlov88@gmail.com>
2023-01-19 17:31:03 +0300

    HAT-33 Вынесение валидации роутов в сервис

--- src/main/kotlin/rubber/dutch/hat/apigateway/filter/SessionFilter.kt
+++ src/main/kotlin/rubber/dutch/hat/apigateway/filter/SessionFilter.kt
@@ -6,28 +6,21 @@ import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
-import rubber.dutch.hat.apigateway.config.SessionFilterConfig
 import rubber.dutch.hat.apigateway.service.SessionService
+import rubber.dutch.hat.apigateway.service.UnsecureRouteService
 
 /**
  * <p>Фильтр сессии. Проверяет наличие Authorization-токена.</p>
  */
 @Component
-class SessionFilter(val sessionService: SessionService, val sessionFilterConfig: SessionFilterConfig) : GlobalFilter {
+class SessionFilter(val sessionService: SessionService, val unsecureRouteService: UnsecureRouteService) : GlobalFilter {
 
     override fun filter(serverWebExchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
-        if (skipValidation(serverWebExchange)) return chain.filter(serverWebExchange)
+        if (unsecureRouteService.isUnsecure(serverWebExchange)) return chain.filter(serverWebExchange)
 
         return sessionService.validate(serverWebExchange)
             .flatMap { exchange -> chain.filter(exchange) }
             .onErrorResume(RuntimeException::class.java) { handleUnauthorized(serverWebExchange) }
-    }
-
-    private fun skipValidation(serverWebExchange: ServerWebExchange): Boolean {
-        val path = serverWebExchange.request.path.value()
-        val method = serverWebExchange.request.method.name()
-
-        return sessionFilterConfig.unsecureRoutes.any { it.path == path && it.method == method }
     }
 
     private fun handleUnauthorized(exchange: ServerWebExchange): Mono<Void> {
---
+++ src/main/kotlin/rubber/dutch/hat/apigateway/service/UnsecureRouteService.kt
@@ -0,0 +1,29 @@
+package rubber.dutch.hat.apigateway.service
+
+import org.springframework.stereotype.Component
+import org.springframework.util.AntPathMatcher
+import org.springframework.web.server.ServerWebExchange
+import rubber.dutch.hat.apigateway.config.SessionFilterConfig
+import rubber.dutch.hat.apigateway.model.Route
+
+@Component
+class UnsecureRouteService(private val sessionFilterConfig: SessionFilterConfig) {
+    companion object {
+        private const val ALL_HTTP_METHODS = "all"
+    }
+
+    fun isUnsecure(serverWebExchange: ServerWebExchange): Boolean {
+        return sessionFilterConfig.unsecureRoutes.any { matchRoute(it, serverWebExchange) }
+    }
+
+
+    private fun matchRoute(route: Route, serverWebExchange: ServerWebExchange): Boolean {
+        val path = serverWebExchange.request.path.value()
+        val method = serverWebExchange.request.method.name()
+
+        val matchPath = AntPathMatcher().match(route.path, path)
+        val matchMethod = route.method == ALL_HTTP_METHODS || route.method == method
+
+        return matchPath && matchMethod
+    }
+}
