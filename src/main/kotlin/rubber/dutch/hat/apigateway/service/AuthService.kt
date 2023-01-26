814bda4a8ceb0c3599557c8151579b95b498d02f
OrlovAnton88 <orlov88@gmail.com>
2023-01-26 17:40:33 +0300

    HAT-33 Экран ожидание игроков
    Новые роуты и веб-сокет соединение

--- src/main/kotlin/rubber/dutch/hat/apigateway/service/AuthService.kt
+++ src/main/kotlin/rubber/dutch/hat/apigateway/service/AuthService.kt
@@ -1,35 +1,24 @@
 package rubber.dutch.hat.apigateway.service
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.ClientResponse
 import org.springframework.web.reactive.function.client.WebClient
-import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Mono
-import rubber.dutch.hat.apigateway.config.HeaderConfig
 import rubber.dutch.hat.apigateway.model.TokenDTO
 
 /**
- * Сервис проверки сессии.
+ * Сервис проверки аутентификации.
  */
 @Service
-class SessionService @Autowired constructor(
-    private val headerConfig: HeaderConfig,
+class AuthService(
     webClientBuilder: WebClient.Builder,
     @Value("\${hat.services.user-service.uri}")
     private var userServiceUri: String,
     @Value("\${hat.services.user-service.tokenPath}")
     private var userServiceTokenPath: String
 ) {
-
-    companion object {
-        /**
-         * Длина префикса "Bearer ", после которого в заголовке запроса следует токен.
-         */
-        const val BEARER_PREFIX_LENGTH = 7
-    }
 
     private val webClient: WebClient
 
@@ -46,9 +35,7 @@ class SessionService @Autowired constructor(
      * @throws IllegalStateException Если токен отсутствует или просрочен.
      * @throws org.springframework.web.reactive.function.client.WebClientResponseException Если получен 4xx ответ.
      */
-    fun validate(exchange: ServerWebExchange): Mono<ServerWebExchange> {
-        val accessToken: String? = getTokenFromHeader(exchange)
-
+    fun validate(accessToken: String?): Mono<TokenDTO> {
         if (accessToken.isNullOrBlank()) {
             return Mono.error { error("Token not found") }
         }
@@ -61,20 +48,5 @@ class SessionService @Autowired constructor(
             .retrieve()
             .onStatus({ httpStatus -> httpStatus.is4xxClientError }, ClientResponse::createException)
             .bodyToMono(TokenDTO::class.java)
-            .map { tokenData -> addSessionHeader(exchange, tokenData) }
-    }
-
-    private fun getTokenFromHeader(exchange: ServerWebExchange): String? {
-        return exchange.request.headers.getFirst(headerConfig.authorization)?.substring(BEARER_PREFIX_LENGTH)
-    }
-
-    private fun addSessionHeader(exchange: ServerWebExchange, tokenData: TokenDTO): ServerWebExchange {
-        if (tokenData.expired) {
-            error("Token expired")
-        }
-
-        val mutatedRequest = exchange.request.mutate().header(headerConfig.userId, tokenData.userId.toString()).build()
-
-        return exchange.mutate().request(mutatedRequest).build()
     }
 }
